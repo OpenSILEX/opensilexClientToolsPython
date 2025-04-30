e95234e69c1f6dcd926913f1cfe45ada982c2b11
Arnaud Charleroy <arnaud.charleroy@inrae.fr>
2025-04-30 13:51:42 +0200

    Add feature/nouha/fix-apis merge

--- opensilexClientToolsPython/api/factors_api.py
+++ opensilexClientToolsPython/api/factors_api.py
@@ -903,7 +903,7 @@ class FactorsApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='list[FactorLevelGetDTO]',  # noqa: E501
+            response_type='FactorLevelGetDTO',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
@@ -1029,7 +1029,7 @@ class FactorsApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='list[FactorLevelGetDetailDTO]',  # noqa: E501
+            response_type='FactorLevelGetDetailDTO',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
